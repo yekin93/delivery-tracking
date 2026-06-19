394b6c3a3134ccdf4422888d394172c0b9affb2d
Yekin Zulfikari <yekin_zulfikar@outlook.com>
2026-06-19 23:35:06 +0200

    feat: add swagger api config

---
+++ src/main/java/com/lrn/delivery_tracking/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.lrn.delivery_tracking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
